35fa5c6fd36c8e2a481bcfc38cbe42e3a0fa13ff
Kevin Thibodeaux <kthib16@gmail.com>
2017-10-01 12:26:36 -0400

    Assignment 7 Bloc Jams Angular completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -29,23 +29,31 @@
             currentSong = song;
          };
 
+         var playSong = function(song){
+            currentBuzzObject.play();
+            song.playing = true;
+         };
+
+         var pauseSong = function(song){
+            currentBuzzObject.pause();
+            song.playing = false;
+         };
+
          SongPlayer.play = function(song) {
              if (currentSong !== song) {
                setSong(song);
-               currentBuzzObject.play();
-               song.playing = true;
+               playSong(song);
              }
 
              else if(currentSong === song) {
                if(currentBuzzObject.isPaused()){
-                 currentBuzzObject.play();
+                 playSong(song);
                }
              }
            };
 
          SongPlayer.pause = function(song){
-            currentBuzzObject.pause();
-            song.playing = false;
+            pauseSong(song);
           };
 
          return SongPlayer;
