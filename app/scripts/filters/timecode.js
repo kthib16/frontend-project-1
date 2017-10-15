e125aa48909937dd547a33191665437019066bfa
Kevin Thibodeaux <kthib16@gmail.com>
2017-10-15 19:48:08 -0400

    Assignment 11 Bloc Jams Angular completed

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -1,28 +1,15 @@
 (function() {
     function timecode() {
-        return function(seconds){
-            var seconds = Number.parseFloat(seconds);
-
-            if(Number.isNaN(seconds)){
-                return '-:--';
-            }
-            var wholeSeconds = Math.floor(seconds);
-            var minutes = Math.floor(wholeSeconds / 60);
-            var remainingSeconds = wholeSeconds % 60;
-
-            var output = minutes + ':';
-
-            if(remainingSeconds < 10){
-              output += '0';
-            }
-
-            output += remainingSeconds;
-
-            return output;
-        };
-    }
+      return function(seconds){
+      if(Number.isNaN(seconds)){
+        return '--:--';
+      }
+      var time = buzz.toTimer(seconds);
+      return time;
+      }
+    };
 
     angular
       .module('blocJams')
-      .filter('timecode', timecode);
+      .filter('timecode', ['SongPlayer', timecode]);
 })();
