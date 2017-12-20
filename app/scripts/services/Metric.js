c74cc2e85c5b4a5617c636e82404bd95daee5096
Alissa Likavec <alissalikavec@gmail.com>
2017-12-19 20:12:49 -0800

    counter changes

--- app/scripts/services/Metric.js
+++ app/scripts/services/Metric.js
@@ -7,7 +7,11 @@
     (function getAllSongs(){
       for(var i = 0; i < Fixtures.albums.length; i++){
         for(var j = 0; j < Fixtures.albums[i].songs.length; j++){
-          $rootScope.songPlays.push(Fixtures.albums[i].songs[j].title);
+          var songTitle = Fixtures.albums[i].songs[j].title;
+          $rootScope.songPlays.push({
+            title: songTitle,
+            plays: 0
+          });
         }
       }
     })();
@@ -15,12 +19,13 @@
     return {
       // Function that keeps track of the number of times each song is played
       counter: function(songObj){
-        var songNumber = 1;
+        var songNumber = 0;
         var albumNumber = 1;
         var artistNumber = 1;
+
         if($rootScope.songPlays){
           for(var i = 0; i < $rootScope.songPlays.length; i++){
-            if($rootScope.songPlays[i] === songObj){
+            if($rootScope.songPlays[i].title === songObj.title){
               songNumber += 1;
             }
           }
@@ -41,16 +46,27 @@
         }
 
         songObj['songPlays'] = songNumber;
+
+        /* storing the number of times an individual song has been played on each song object is okay,
+        but we don't really need every song object to contain the number of album and artist plays.
+        consider only having this data on the rootScope. */
+
         songObj['albumPlays'] = albumNumber;
         songObj['artistPlays'] = artistNumber;
+        console.log("songPlays is " + songObj['songPlays'] + ", albumPlays is " + songObj['albumPlays'] + " artistPlays is " + songObj['artistPlays']);
       },
       // Function that records a metric object by pushing it to the $rootScope array
       registerSongPlay: function(songObj) {
         // Add time to event register
         songObj['playedAt'] = new Date();
-        console.log(songObj);
-        console.log($rootScope.songPlays);
-        $rootScope.songPlays.push(songObj);
+        $rootScope.songPlays.some(function(song){
+          if(song.title === songObj.title){
+            song.plays += 1;
+            console.log("Added one to " + song.title);
+            console.log($rootScope.songPlays);
+            return true;
+          }
+        });
       },
       registerAlbumInfo: function(songObj){
         songObj['artist'] = $rootScope.album.artist;
